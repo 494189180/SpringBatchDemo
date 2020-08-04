bdf1b78568454152559e667be0bade47b183cf46
gaoyanbao <gaoyanbao@csii.com.cn>
2020-08-04 11:48:39 +0800

    注释启动注解修改数据库配置保证启动服务正常

--- src/main/java/com/example/springbatch/jobOperator/SpringbatchApplication.java
+++ src/main/java/com/example/springbatch/jobOperator/SpringbatchApplication.java
@@ -3,7 +3,7 @@ package com.example.springbatch.jobOperator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class SpringbatchApplication {
 
     public static void main(String[] args) {
